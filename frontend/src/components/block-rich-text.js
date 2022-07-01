2d8efb45ef5aa7971284c4682ad05435499d1d54
jaewoo <jaewoo986@gmail.com>
2022-07-01 17:56:50 +0900

    [frontend] 포스트의 헤더&rich text 너비 수정.

--- frontend/src/components/block-rich-text.js
+++ frontend/src/components/block-rich-text.js
@@ -2,12 +2,14 @@ import React from "react"
 
 const BlockRichText = ({ data }) => {
   return (
-    <div className="prose mx-auto py-8">
-      <div
-        dangerouslySetInnerHTML={{
-          __html: data.richTextBody.data.childMarkdownRemark.html,
-        }}
-      />
+    <div className={"container"}>
+      <div className="prose mx-auto max-w-none py-8">
+        <div
+          dangerouslySetInnerHTML={{
+            __html: data.richTextBody.data.childMarkdownRemark.html,
+          }}
+        />
+      </div>
     </div>
   )
 }
