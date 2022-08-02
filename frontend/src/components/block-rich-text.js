424a7811b443ff6aba4597e8a9bc2347a6a133b8
jaewoo <jaewoo986@gmail.com>
2022-08-02 16:49:33 +0900

    Revert "[frontend] 포스트의 헤더&rich text 너비 수정."
    This reverts commit 2d8efb45ef5aa7971284c4682ad05435499d1d54.

--- frontend/src/components/block-rich-text.js
+++ frontend/src/components/block-rich-text.js
@@ -2,14 +2,12 @@ import React from "react";
 
 const BlockRichText = ({ data }) => {
   return (
-    <div className={"container"}>
-      <div className="prose mx-auto max-w-none py-8">
-        <div
-          dangerouslySetInnerHTML={{
-            __html: data.richTextBody.data.childMarkdownRemark.html,
-          }}
-        />
-      </div>
+    <div className="prose mx-auto py-8">
+      <div
+        dangerouslySetInnerHTML={{
+          __html: data.richTextBody.data.childMarkdownRemark.html,
+        }}
+      />
     </div>
   );
 };
