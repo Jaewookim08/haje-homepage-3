4131223c69088025f4616afe3f7bf6d52f42bd77
jaewoo <jaewoo986@gmail.com>
2022-08-02 16:20:43 +0900

    [frontend/navbar] Add hover effect to links.

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = ({ isFrontPage }: Props) => {
 
   return (
     <header
-      className={`fixed z-50 h-20 w-full transition-colors duration-300 ease-in-out ${
+      className={`sticky z-50 h-20 w-full transition-colors duration-300 ease-in-out ${
         isFrontPage
           ? "bg-transparent text-white hover:bg-neutral-50 hover:text-black"
           : "bg-neutral-50 text-black"
@@ -40,10 +40,18 @@ const Navbar = ({ isFrontPage }: Props) => {
         >
           HAJE
         </Link>
-        <div className="flex h-full flex-row items-baseline items-stretch justify-between gap-x-20">
+        <div className="flex h-full flex-row items-baseline items-stretch justify-between">
           {links.map(({ to, inner }) => (
-            <Link className="medium flex items-center" to={to}>
-              {inner}
+            <Link
+              className="medium inline-block flex items-center px-12 [&>*]:hover:text-primary [&>*]:hover:after:origin-bottom-left [&>*]:hover:after:scale-x-100"
+              to={to}
+            >
+              <span
+                className={`relative inline-block transition-colors duration-200 ease-out after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full after:origin-bottom-right 
+                after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 after:ease-out after:content-['']`}
+              >
+                {inner}
+              </span>
             </Link>
           ))}
         </div>
